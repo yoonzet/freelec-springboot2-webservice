fcaec47a709a7f53a3aa72858854433774cd238d
yoonji lee <lalala703@naver.com>
2022-12-20 22:02:03 +0900

    1220

--- src/main/java/com/springboot/practice/Application.java
+++ src/main/java/com/springboot/practice/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application { //이 클래스는 앞으로 만들 프로젝트의 메인클래스가 된다.
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/com/springboot/practice/web/HelloController.java
+++ src/main/java/com/springboot/practice/web/HelloController.java
@@ -1,12 +1,14 @@
 package com.springboot.practice.web;
 
+import com.springboot.practice.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 준다.
 public class HelloController {
-    @GetMapping("/hello") // get의 요청을 받을 수 있는 API를 만들어 준다. /hello는 경로가 된다.
-    public String hello(){
-        return "hello";
+    @GetMapping("/hello/dto") // get의 요청을 받을 수 있는 API를 만들어 준다. /hello는 경로가 된다.
+    public HelloResponseDto helloDto(@RequestParam("name") String name,@RequestParam("amount")int amount){
+        return new HelloResponseDto(name, amount);
     }
 }
