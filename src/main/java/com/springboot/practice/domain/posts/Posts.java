422620175a525d9e206e6d20eca9d5e3b6170189
yoonji lee <lalala703@naver.com>
2023-01-02 15:47:11 +0900

    0102

---
+++ src/main/java/com/springboot/practice/domain/posts/Posts.java
@@ -0,0 +1,28 @@
+package com.springboot.practice.domain.posts;
+
+import lombok.Builder;
+
+import javax.persistence.*;
+
+@Entity
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
+
